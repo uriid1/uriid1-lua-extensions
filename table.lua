7c12e9386b8cbb4567a2fb35e34866386b8a6ad4
uriid1 <appdurov@gmail.com>
2023-07-30 23:36:13 +0300

    Add table merge and compare

--- table.lua
+++ table.lua
@@ -71,10 +71,52 @@ end
 -- Merging table t2 into t1 (is not deep copy)
 -- @param t1 Table
 -- @param t2 Table
-local function merge(t1, t2)
-  for k, v in next, t2 do
-    t1[k] = v
+function M.merge(t1, t2)
+  for key, value in pairs(t2) do
+    if type(value) == 'table' and type(t1[key]) == 'table' then
+      M.merge(t1[key], t2[key])
+    else
+      t1[key] = value
+    end
   end
+
+  return t1
+end
+
+---
+-- Compare t1 and t2
+-- @param t1 Table
+-- @param t2 Table
+function M.compare(t1, t2)
+  -- Check element count
+  local count1 = 0
+  for _ in pairs(t1) do
+    count1 = count1 + 1
+  end
+
+  local count2 = 0
+  for _ in pairs(t2) do
+    count2 = count2 + 1
+  end
+
+  if count1 ~= count2 then
+    return false
+  end
+
+  -- Check key value
+  for key, value in pairs(t1) do
+    if type(value) == 'table' then
+      if type(t2[key]) ~= 'table' or not M.compare(value, t2[key]) then
+        return false
+      end
+    else
+      if value ~= t2[key] then
+        return false
+      end
+    end
+  end
+
+  return true
 end
 
 ---
