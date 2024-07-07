7ac9f9cc729353358e96b3d1e29b8d5e200b5c2d
uriid1 <appdurov@gmail.com>
2024-07-07 03:34:54 +0300

    Update ext test

--- src/extensions/test.lua
+++ src/extensions/test.lua
@@ -4,6 +4,42 @@ local M = {}
 
 local unpack = unpack or table.unpack
 
+local DEFAULT_MAX_SEEN = 1000
+
+-- Поиск имени функции по адресу
+--
+local seenArg = {}
+
+local function findNameByAddr(addr, level)
+  if seenArg[addr] then
+    return seenArg[addr]
+  end
+
+  -- Find local name by addr
+  for i = 1, DEFAULT_MAX_SEEN do
+    local name, value = debug.getlocal(level, i)
+    if not name and not value then
+      break
+    end
+
+    if value == addr then
+      seenArg[addr] = name
+      return name
+    end
+  end
+
+  -- Find global name by addr
+  for name, value in pairs(_G) do
+    if value == addr then
+      seenArg[addr] = name
+      return name
+    end
+  end
+
+  return nil
+end
+--
+
 --- Тестирует скорость выполнения функции
 -- @param param Параметры
 -- @param param.func Тестируемая функция
@@ -17,8 +53,23 @@ function M.bench(param)
   local summary
   for _ = 1, param.count do
     local start = os.clock()
-    param.func(unpack(param.args))
+    if param.args then
+      param.func(unpack(param.args))
+    else
+      param.func()
+    end
     summary = os.clock() - start
+  end
+
+  if param and param.print then
+    local level = param.level or 2
+    local name = findNameByAddr(param.func, level+1) or 'nil'
+    print(
+      string.format(
+        'Function: %s | Count %d | Duration %s',
+        name, param.count, summary
+        )
+    )
   end
 
   return summary
